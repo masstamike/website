a08552eb59338f12e79defc4bf467be8bf5dca23
Michael Sawyer <michaelsawyer92@gmail.com>
2016-09-22 21:21:55 -0700

    Convert everything to jsx

---
+++ public/react/js/components/main.react.js
@@ -0,0 +1,54 @@
+var Header = React.createClass({
+    render: function() {
+        
+        return (
+            <h1>"Welcome to Michael A. Sawyer's React Page!"</h1>
+        );
+    }
+});
+
+var Body = React.createClass({
+    
+    render: function() {
+        return (
+            <div>
+                <ul className="nav nav-tabs"></ul>
+                //React.createElement(Tabs, null)
+            </div>
+        );
+    }
+});
+
+var Footer = React.createClass({
+
+    render: function() {
+        return (
+            <div id="footer">
+                <div className="center">
+                Questions or comments? Send them to 
+                    <a href="mailto:michaelsawyer92@gmail.com">"Michael Sawyer"</a><br/>
+                    <a href="/Resume.pdf">"Resume"</a>
+                    <a href="/RecLetter.pdf">"Letter of Recommendation"</a>
+                    <a href="http://www.linkedin.com/in/michaelasawyer">"LinkedIn"</a>
+                </div>
+            </div>
+        );
+    }
+});
+
+var Main = React.createClass({
+    
+    render: function() {
+        
+        console.log("Building the website...");
+        
+        return (
+            <div>
+                <Header/>
+                <Body/>
+                <Footer/>
+            </div>
+        );
+    }
+    
+});
